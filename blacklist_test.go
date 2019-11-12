0d015f5824f59a4ee3a54f9c0cce1211b593e916
Sergio Miranda <sjamr10@gmail.com>
2019-11-12 13:50:37 -0300

    Add subtest and test helper function assertStrings

--- blacklist_test.go
+++ blacklist_test.go
@@ -3,10 +3,20 @@ package blacklist
 import "testing"
 
 func TestReplace(t *testing.T) {
-	text := "Text"
-	blacklist := "Text"
-	want := ""
-	if got := Replace(text, blacklist); got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
+	t.Run("text is the same as blacklist", func(t *testing.T) {
+		text := "Text"
+		blacklist := "Text"
+		got, _ := Replace(text, blacklist)
+		want := ""
+
+		assertStrings(t, got, want)
+	})
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
